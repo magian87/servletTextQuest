550f57a64e5209cf3c991ad86b1bf3f2f5d853af
magian87 <Valiakhmetovbz@gmail.com>
2022-09-20 16:19:02 +0500

    add files

---
+++ src/test/Java/ru/servlettextquest_/classes/InitGameTest.java
@@ -0,0 +1,13 @@
+package ru.servlettextquest_.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InitGameTest {
+
+    @Test
+    public void receiveRooms() {
+        assertTrue(true);
+    }
+}
